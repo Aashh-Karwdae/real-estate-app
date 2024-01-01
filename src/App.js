6f98b117aa8399f026fe972768a942024431ee94
Aashh karwade <ashwinkarwade54321@gmail.com>
2024-01-01 13:59:30 +0530

    create the header component and link it with pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from './Pages/Home'
 import About from './Pages/About'
 import SignIn from './Pages/SignIn'
 import {Routes,Route, BrowserRouter} from "react-router-dom";
+import Header from './Components/Header';
 
 export default function App() {
   return (
     <div>
     <BrowserRouter>
+    <Header/>
     <Routes>
     <Route path="/Home" element={<Home/>}/>
     <Route path="/About" element={<About/>}/>
